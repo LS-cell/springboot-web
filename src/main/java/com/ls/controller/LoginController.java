c186650a9a9b30dd5233c4e82b60a5e636861ce4
LS-cell <371471895@qq.com>
2020-11-06 14:51:49 +0800

    修复登录成功页面跳转错误的问题

--- src/main/java/com/ls/controller/LoginController.java
+++ src/main/java/com/ls/controller/LoginController.java
@@ -19,7 +19,7 @@ public class LoginController {
         if(!StringUtils.isEmpty(username) && "123".equals(password)){
             //登陆成功，防止表单重复提交，可以重定向到主页
             session.setAttribute("loginUser",username);
-            return "redirect:/main.html";
+            return "dashboard";
         }else{
             //登陆失败
             map.put("msg","用户名密码错误");
