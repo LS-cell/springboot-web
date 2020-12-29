d9a77a414a82e3ead43630e6ff25471a152e4dfc
lilei17 <lilei17@asiainfo.com>
2020-12-29 18:00:11 +0800

    正常提交

--- src/main/java/com/ls/controller/ImportController.java
+++ src/main/java/com/ls/controller/ImportController.java
@@ -3,6 +3,8 @@ package com.ls.controller;
 import com.ls.pojo.Employee;
 import com.ls.service.impl.EmployeeSvImpl;
 import com.ls.util.ImportToExcel;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,8 @@ import java.util.List;
 
 @Controller
 public class ImportController {
+
+    private static final Log log = LogFactory.getLog(ImportController.class);
 
     @Autowired
     EmployeeSvImpl employeeSv;
